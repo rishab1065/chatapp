3a7d10ea233a4c797797aca4165ff73954e66f0b
Rishab Jain <rishab1065@gmail.com>
2020-11-06 13:35:58 +0530

    added chat message UI

--- src/components/Tabs/Chat/Chat.js
+++ src/components/Tabs/Chat/Chat.js
@@ -4,8 +4,24 @@ import './Chat.css';
 export default function Chat() {
   return (
     <div className="chat_container">
+      <div className="chat_messages_container">
+        <div className="chat_messages">
+          <div className="chat-message right">HI</div>
+          <div className="chat-message left">HI</div>
+          <div className="chat-message right">HI</div>
+          <div className="chat-message right">HI</div>
+          <div className="chat-message right">HI</div>
+          <div className="chat-message right">HI</div>
+          <div className="chat-message right">HI</div>
+          <div className="chat-message right">HI</div>
+          <div className="chat-message right">HI</div>
+          <div className="chat-message right">HI</div>
+          <div className="chat-message right">HI</div>
+          <div className="chat-message right">HI</div>
+        </div>
+      </div>
       <div className="chat_footer">
-        <div className="chat_message">
+        <div className="chat_message_input_container">
           <input className="chat_message_input" placeholder={'Type message'} />
           <div className="chat_send_button">
             <i class="material-icons">{'send'}</i>
